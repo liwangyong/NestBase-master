7bb4b0bd71194de074977a28031150a07acaaf94
ADMAIN\admin <18260439083@163.com>
2019-11-20 21:45:36 +0800

    提交rabbit服务

--- src/preInit.ts
+++ src/preInit.ts
@@ -1,58 +1,8 @@
 import {
   INestApplication,
-  HttpException,
-  Injectable,
-  Inject,
 } from '@nestjs/common';
-import { JournalArrayServiceDto } from './dto/service-dto/journal-dto';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as amqp from 'amqplib/callback_api';
-import { env } from './until/env-unit';
-import { LoggerExtService } from './services/entities/logger-service';
-@Injectable()
 export class PreInit {
-  amqp: any = amqp;
-  channelSendToQueue: any;
-  queue: string = 'guest';
-  constructor(
-    @Inject(LoggerExtService)
-    private readonly loggerExtService: LoggerExtService,
-  ) {
-    this.Initialization();
-  }
-  Initialization() {
-    this.amqp.connect(env('NEST_RABBITMQ'), this.createChannel.bind(this));
-  }
-  createChannel(error0, connection) {
-    if (error0) {
-      setTimeout(() => this.Initialization(), 0);
-      throw new HttpException({ error: '连接rabbit失败' }, 500);
-    }
-    connection.createChannel((error1, channel) => {
-      if (error1) {
-        setTimeout(() => this.Initialization(), 0);
-        throw new HttpException({ error: '连接rabbit失败' }, 500);
-      }
-      const { queue } = this;
-      channel.assertQueue(queue, {
-        durable: false,
-      });
-      channel.consume(queue, this.channelConsume.bind(this), {
-        noAck: true,
-      });
-      this.channelSendToQueue = channel.sendToQueue.bind(channel);
-      global['rabbitMqSend'] = channel.sendToQueue.bind(channel);
-      //   this.rabSendToQueue('我是你爸爸');
-    });
-  }
-  async channelConsume(ctn: any) {
-    const msg = ctn.content.toString();
-    const data = await this.loggerExtService.findAll();
-    console.log(' [x] Received %s', JSON.parse(msg), data);
-  }
-  rabSendToQueue(msg: JournalArrayServiceDto) {
-    this.channelSendToQueue(this.queue, Buffer.from(JSON.stringify(msg)));
-  }
   generatorSwagger(app: INestApplication) {
     const swaggerOptions = new DocumentBuilder()
       .setTitle('nestjs')
--- src/services/microService/rabbitmq-server.ts
+++ src/services/microService/rabbitmq-server.ts
@@ -1,17 +1,58 @@
-import * as amqp from 'amqplib'
-import { Injectable } from '@nestjs/common'
-import { defaultSelfInfo } from '../../convertConfig/ambient'
-import { JournalArrayServiceDto } from '../../dto/service-dto/journal-dto'
+import {
+  HttpException,
+  Injectable,
+  Inject,
+} from '@nestjs/common';
+import { JournalArrayServiceDto } from '../../dto/service-dto/journal-dto';
+import * as amqp from 'amqplib/callback_api';
+import { env } from '../../until/env-unit';
+import { ConfigService } from 'nestjs-config';
+import { LoggerExtService } from '../../services/entities/logger-service';
 @Injectable()
-export class RabbitMqMicroService {
-  amqp: any = amqp
-  conn: any
-  constructor() { }
-  amqpConnectCreateChannel(info: JournalArrayServiceDto) {
-    defaultSelfInfo(async data => {
-      const conn = await this.amqp.connect(data.rabbitmq.url)
-      this.sendToQueue = await conn.createChannel()
-    })
+export class PreInit {
+  amqp: ConfigService = amqp;
+  channelSendToQueue: any;
+  queue: string = 'guest';
+  constructor(
+    @Inject(LoggerExtService)
+    private readonly loggerExtService: LoggerExtService,
+  ) {
+    this.Initialization();
   }
-  sendToQueue() { }
+  // 连接
+  Initialization() {
+    this.amqp.connect(env('NEST_RABBITMQ'), this.createChannel.bind(this));
+  }
+  // 连接回调
+  createChannel(error0, connection) {
+    if (error0) {
+      setTimeout(() => this.Initialization(), 0);
+      throw new HttpException({ error: '连接rabbit失败' }, 500);
+    }
+    connection.createChannel((error1, channel) => {
+      if (error1) {
+        setTimeout(() => this.Initialization(), 0);
+        throw new HttpException({ error: '连接rabbit失败' }, 500);
+      }
+      const { queue } = this;
+      channel.assertQueue(queue, {
+        durable: false,
+      });
+      channel.consume(queue, this.channelConsume.bind(this), {
+        noAck: true,
+      });
+      this.channelSendToQueue = channel.sendToQueue.bind(channel);
+      global['rabbitMqSend'] = channel.sendToQueue.bind(channel);
+    });
+  }
+  // 监听队列
+  async channelConsume(ctn: any) {
+    const msg = ctn.content.toString();
+    const data = await this.loggerExtService.findAll();
+    console.log(' [x] Received %s', JSON.parse(msg), data);
+  }
+  // 发送队列
+  rabSendToQueue(msg: JournalArrayServiceDto) {
+    this.channelSendToQueue(this.queue, Buffer.from(JSON.stringify(msg)));
+  }
 }

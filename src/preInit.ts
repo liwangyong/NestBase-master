274c21832c339444095e4ab3e357dc7a60e19d6a
wangyong.li <wangyong.li@yunlsp.com>
2019-11-20 12:32:04 +0800

    修改

--- src/preInit.ts
+++ src/preInit.ts
@@ -1,50 +1,57 @@
-import { INestApplication, HttpException, Injectable, Inject } from '@nestjs/common'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import * as amqp from 'amqplib/callback_api'
-import { env } from './until/env-unit'
-import { LoggerExtService } from "./services/entities/logger-service"
+import {
+  INestApplication,
+  HttpException,
+  Injectable,
+  Inject,
+} from '@nestjs/common';
+import { JournalArrayServiceDto } from './dto/service-dto/journal-dto';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as amqp from 'amqplib/callback_api';
+import { env } from './until/env-unit';
+import { LoggerExtService } from './services/entities/logger-service';
 @Injectable()
 export class PreInit {
-  amqp: any = amqp
-  channelSendToQueue: any
-  queue: string = 'guest'
+  amqp: any = amqp;
+  channelSendToQueue: any;
+  queue: string = 'guest';
   constructor(
-    @Inject(LoggerExtService) private readonly loggerExtService: LoggerExtService
+    @Inject(LoggerExtService)
+    private readonly loggerExtService: LoggerExtService,
   ) {
-    this.Initialization()
+    this.Initialization();
   }
   Initialization() {
-    this.amqp.connect(env('NEST_RABBITMQ'), this.createChannel.bind(this))
+    this.amqp.connect(env('NEST_RABBITMQ'), this.createChannel.bind(this));
   }
   createChannel(error0, connection) {
     if (error0) {
-      setTimeout(() => this.Initialization(), 0)
-      throw new HttpException({ error: '连接rabbit失败' }, 500)
+      setTimeout(() => this.Initialization(), 0);
+      throw new HttpException({ error: '连接rabbit失败' }, 500);
     }
     connection.createChannel((error1, channel) => {
       if (error1) {
-        setTimeout(() => this.Initialization(), 0)
-        throw new HttpException({ error: '连接rabbit失败' }, 500)
+        setTimeout(() => this.Initialization(), 0);
+        throw new HttpException({ error: '连接rabbit失败' }, 500);
       }
-      const { queue } = this
+      const { queue } = this;
       channel.assertQueue(queue, {
         durable: false,
-      })
+      });
       channel.consume(queue, this.channelConsume.bind(this), {
         noAck: true,
-      })
-      this.channelSendToQueue = channel.sendToQueue.bind(channel)
-      global["rabbitMqSend"] = channel.sendToQueue.bind(channel)
-      this.rabSendToQueue("我是你爸爸")
-    })
+      });
+      this.channelSendToQueue = channel.sendToQueue.bind(channel);
+      global['rabbitMqSend'] = channel.sendToQueue.bind(channel);
+      //   this.rabSendToQueue('我是你爸爸');
+    });
   }
   async channelConsume(ctn: any) {
-    const msg = ctn.content.toString()
-    const data = await this.loggerExtService.findAll()
-    console.log(' [x] Received %s', JSON.parse(msg), data)
+    const msg = ctn.content.toString();
+    const data = await this.loggerExtService.findAll();
+    console.log(' [x] Received %s', JSON.parse(msg), data);
   }
-  rabSendToQueue(msg) {
-    this.channelSendToQueue(this.queue, Buffer.from(JSON.stringify(msg)))
+  rabSendToQueue(msg: JournalArrayServiceDto) {
+    this.channelSendToQueue(this.queue, Buffer.from(JSON.stringify(msg)));
   }
   generatorSwagger(app: INestApplication) {
     const swaggerOptions = new DocumentBuilder()
@@ -53,8 +60,8 @@ export class PreInit {
       .setVersion('1.0')
       .addBearerAuth()
       .addTag('cats')
-      .build()
-    const swaggerDoc = SwaggerModule.createDocument(app, swaggerOptions)
-    SwaggerModule.setup('swagger', app, swaggerDoc)
+      .build();
+    const swaggerDoc = SwaggerModule.createDocument(app, swaggerOptions);
+    SwaggerModule.setup('swagger', app, swaggerDoc);
   }
 }

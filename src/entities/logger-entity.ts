8be1953ada903b3a2437f280f2bce6f435df48e7
wangyong.li <wangyong.li@yunlsp.com>
2019-11-22 16:14:22 +0800

    添加索引

--- src/entities/logger-entity.ts
+++ src/entities/logger-entity.ts
@@ -1,17 +1,21 @@
-import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, BeforeInsert} from 'typeorm'
+import {Entity, Column, PrimaryGeneratedColumn, Index} from 'typeorm'
 import {Incorrect} from '../constants/incorrect-constants'
 import {BasicEntity} from './basics/basics-entity'
 // console.log(typeorm);
 @Entity()
 export class LoggerExtEntity extends BasicEntity {
 	@PrimaryGeneratedColumn('uuid') // 主键
-	uuid: string
+    uuid: string
+    @Index()
 	@Column({type: 'varchar', comment: '错误等级', default: Incorrect.Info})
-	level: string
+    level: string
+    @Index()
 	@Column({type: 'bigint', comment: '创建时间'})
-	createdTime: Date
+    createdTime: Date
+    @Index()
 	@Column({type: 'varchar', comment: '错误地址/接口'})
-	url: string
+    url: string
+    @Index()
 	@Column({type: 'varchar', comment: '操作者'})
 	operator: string
 	@Column({type: 'varchar', comment: '日志内容'})

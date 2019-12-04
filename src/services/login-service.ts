e65ca52a599a761b7498e1ef7d2e83d6cc22f734
wangyong.li <wangyong.li@yunlsp.com>
2019-12-04 18:21:53 +0800

    sso登录效验

--- src/services/login-service.ts
+++ src/services/login-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { LoginServiceDto } from '../dto/service-dto/login-dto'
 import { ResultSend } from '../dto/result-dto';
 import axios from 'axios'
+import { loginPullOut } from '../until/private-information'
 
 @Injectable()
 export class LoginService {
@@ -12,30 +13,19 @@ export class LoginService {
   }
   async loginVerification(req: LoginServiceDto): Promise<ResultSend> {
     const { account, password } = req
-    const {data: res} = await this.axios({
-      method: 'post',
-      url: 'http://manage.yunlsp.com/unified/doLogin',
-      params: {
-        userName: account,
-        password,
-      },
-    })
-    const { msg, data , success } = res
+    const res = await loginPullOut(account, password)
+    const { msg, success } = res
     let code: number = 0
     let message: string = ''
     let content: string | any = ''
     if (success) {
       code = 200
       message = 'success'
-      content = {
-        userId: msg,
-        data,
-      }
     } else {
       code = 400
       message = 'error'
-      content = msg
     }
+    content = msg
     return { code, message, content }
   }
 }
--- src/until/private-information.ts
+++ src/until/private-information.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
+import { ResultSend } from '../dto/result-dto';
 export const accessToPrivate = async (sessionId: string): Promise<AxiosResponse> => {
   return await axios({
     url: 'http://manage.yunlsp.com/unified/api/user/info',
@@ -6,4 +7,16 @@ export const accessToPrivate = async (sessionId: string): Promise<AxiosResponse>
       sessionId,
     },
   })
+}
+
+export const loginPullOut = async (userName: string, password: string | number): Promise<any> => {
+  const { data: res } = await axios({
+    method: 'post',
+    url: 'http://manage.yunlsp.com/unified/doLogin',
+    params: {
+      userName,
+      password,
+    },
+  })
+  return res
 }
